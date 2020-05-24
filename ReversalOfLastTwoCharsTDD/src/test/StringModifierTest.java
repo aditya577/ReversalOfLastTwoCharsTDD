d6d26aaf6b0b6e719a6b54d9765635237af89cbd
aditya <aditya@Warlord-pc>
2020-05-24 15:08:12 +0530

    Added a new test testWith10Chars() and got GREEN

--- ReversalOfLastTwoCharsTDD/src/test/StringModifierTest.java
+++ ReversalOfLastTwoCharsTDD/src/test/StringModifierTest.java
@@ -7,17 +7,23 @@ import org.junit.jupiter.api.Test;
 import main.StringModifier;
 
 class StringModifierTest {
+	
+	StringModifier sm = new StringModifier();
 
 	@Test
 	void testWith2Chars() {
-		StringModifier sm = new StringModifier();
 		assertEquals("BA",sm.reverseLast2Chars("AB"));
 	}
 	
 	@Test
 	void testWith4Chars() {
-		StringModifier sm = new StringModifier();
 		assertEquals("ABDC",sm.reverseLast2Chars("ABCD"));
 	}
+	
+	@Test
+	void testWith10Chars() {
+		assertEquals("ABCDEFGHJI",sm.reverseLast2Chars("ABCDEFGHIJ"));
+	}
+	
 
 }
