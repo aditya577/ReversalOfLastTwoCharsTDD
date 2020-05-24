c477c052bf47cb7c6988e0e38db247b385efff87
aditya <aditya@Warlord-pc>
2020-05-24 14:56:00 +0530

    modified main class for testWith2Chars() method, and the is green

--- ReversalOfLastTwoCharsTDD/src/main/StringModifier.java
+++ ReversalOfLastTwoCharsTDD/src/main/StringModifier.java
@@ -3,8 +3,10 @@ package main;
 public class StringModifier {
 
 	public String reverseLast2Chars(String str) {
+		char firstChar = str.charAt(0);
+		char secondChar = str.charAt(1);
 		
-		return null;
+		return ""+secondChar+firstChar;
 	}
 
 }
