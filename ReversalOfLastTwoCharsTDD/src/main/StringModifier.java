86afe194ed12c1e6c1de1885a6e87183f0f96dd0
aditya <aditya@Warlord-pc>
2020-05-24 15:06:20 +0530

    modified the main class a little more and got GREEN for testWith4Chars()

--- ReversalOfLastTwoCharsTDD/src/main/StringModifier.java
+++ ReversalOfLastTwoCharsTDD/src/main/StringModifier.java
@@ -5,11 +5,12 @@ public class StringModifier {
 	public String reverseLast2Chars(String str) {
 		int len = str.length();
 		
+		String strMinusLast2Chars = str.substring(0, len-2);
 		
 		char secondLastChar = str.charAt(len-2);
 		char lastChar = str.charAt(len-1);
 		
-		return ""+lastChar+secondLastChar;
+		return strMinusLast2Chars+lastChar+secondLastChar;
 	}
 
 }
